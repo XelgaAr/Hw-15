bf9db39ada8953684e1cefa623f426133642674c
XelgaAr <143228153+XelgaAr@users.noreply.github.com>
2026-06-03 19:59:46 +0300

    second commit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 import './style.css'
+
+const themeToggleBtn = document.getElementById('theme-toggle')
+
+if (themeToggleBtn) {
+  themeToggleBtn.addEventListener('click', () => {
+    document.documentElement.classList.toggle('dark')
+  })
+}
