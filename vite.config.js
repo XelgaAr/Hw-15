ce6fc3dfb07141242455b73423c5f44998458fd7
XelgaAr <143228153+XelgaAr@users.noreply.github.com>
2026-06-03 19:12:09 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [
+    tailwindcss()
+  ],
+  build: {
+    outDir: 'dist',
+    cssCodeSplit: true,
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  }
+})
